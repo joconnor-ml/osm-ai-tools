6838c3039b6dec128f99af8ec3e16c5655f18810
Joseph O'Connor <joe@energyandcleanair.org>
2020-12-31 11:03:42 +0000

    Add RESISC notebook

--- scripts/generate_object_location_data.py
+++
@@ -1,9 +0,0 @@
-from osm_object_detector import query_osm
-
-if __name__ == "__main__":
-    df = query_osm.query_objects([
-        query_osm.ObjectQuery(tag_key="man_made", tag_value="cooling_tower", object_class="cooling_tower"),
-        query_osm.ObjectQuery(tag_key="tower:type", tag_value="cooling", object_class="cooling_tower"),
-    ])
-    print(f"{df.shape[0]} usable results")
-    df.to_csv("data/object_location_data.csv", index=False)
