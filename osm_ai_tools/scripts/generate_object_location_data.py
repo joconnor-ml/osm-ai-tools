0ac373b65a3cea5c6e0d8035947e6072b2085fb8
Joseph O'Connor <joe@energyandcleanair.org>
2021-03-03 10:35:17 +0000

    Make --include-tags a flag

--- osm_ai_tools/scripts/generate_object_location_data.py
+++ osm_ai_tools/scripts/generate_object_location_data.py
@@ -7,7 +7,7 @@ from osm_ai_tools import query_osm
 @click.command()
 @click.option('--query-config', help='JSON config file', required=True, type=str)
 @click.option('--output-csv', help='Path to output object location CSV', required=True, type=str)
-@click.option('--include-tags', help='Write all way tags to output dataframe', required=False, type=bool, default=False)
+@click.option('--include-tags', help='Write all way tags to output dataframe', is_flag=True)
 def cli(query_config, output_csv, include_tags):
     with open(query_config, "rt") as f:
         conf = json.load(f)
