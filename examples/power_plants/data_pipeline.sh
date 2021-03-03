b7c1fce081307cd18a7a8169bf38bd1a439f76f7
Joseph O'Connor <joe@energyandcleanair.org>
2021-03-03 10:31:42 +0000

    Add tags to download

--- examples/power_plants/data_pipeline.sh
+++ examples/power_plants/data_pipeline.sh
@@ -1,6 +1,6 @@
 DATA_DIR=data/power_plants
 mkdir -p ${DATA_DIR}/images ${DATA_DIR}/tfrecords && \
-generate_object_location_data --query-config examples/power_plants/query.json --output-csv ${DATA_DIR}/object_location_data.csv && \
+generate_object_location_data --query-config examples/power_plants/query.json --output-csv ${DATA_DIR}/object_location_data.csv --include-tags && \
 cluster_objects --input-objects ${DATA_DIR}/object_location_data.csv --output-clusters ${DATA_DIR}/clusters.csv --output-objects ${DATA_DIR}/object_location_data_clustered.csv && \
 download_images --input-csv ${DATA_DIR}/clusters.csv --image-dir ${DATA_DIR}/images --output-csv ${DATA_DIR}/images.csv && \
 generate_bboxes --input-image-csv ${DATA_DIR}/images.csv --input-object-csv ${DATA_DIR}/object_location_data_clustered.csv --output-csv ${DATA_DIR}/bboxes.csv
