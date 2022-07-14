c6bc95d76e1b1acc3ef2badad73d887c353dd708
Joe <joe@transitionzero.org>
2022-07-14 15:33:13 +0100

    Allow calling scripts from command line

--- setup.py
+++ setup.py
@@ -5,17 +5,14 @@ setup(
     version="0.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        "Click",
-    ],
     entry_points="""
         [console_scripts]
-        download_images=osm_ai_tools.scripts.download_images:cli
-        generate_bboxes=osm_ai_tools.scripts.generate_bboxes:cli
-        generate_object_location_data=osm_ai_tools.scripts.generate_object_location_data:cli
-        cluster_objects=osm_ai_tools.scripts.cluster_objects:cli
-        generate_tfrecords=osm_ai_tools.scripts.generate_tfrecords:cli
-        custom_location_data=osm_ai_tools.scripts.custom_location_data:cli
-        data_pipeline=osm_ai_tools.scripts.data_pipeline:cli
+        download_images=osm_ai_tools.scripts.download_images:main
+        generate_bboxes=osm_ai_tools.scripts.generate_bboxes:main
+        generate_object_location_data=osm_ai_tools.scripts.generate_object_location_data:main
+        cluster_objects=osm_ai_tools.scripts.cluster_objects:main
+        generate_tfrecords=osm_ai_tools.scripts.generate_tfrecords:main
+        custom_location_data=osm_ai_tools.scripts.custom_location_data:main
+        data_pipeline=osm_ai_tools.scripts.data_pipeline:main
     """,
 )
