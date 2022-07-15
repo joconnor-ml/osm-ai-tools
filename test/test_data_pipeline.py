788e4ad083b16b3fd96d97354e437c58001c2b96
Joe <joe@transitionzero.org>
2022-07-15 09:49:44 +0100

    Add quick integration test

--- test/test_data_pipeline.py
+++ test/test_data_pipeline.py
@@ -1,7 +1,10 @@
 """Quick integration test of entire data pipeline"""
 from osm_ai_tools.scripts import data_pipeline
 import unittest
+import os
+
+CONFIG_FILE = os.path.join(os.path.dirname(__file__), "config.json")
 
 class TestPipeline(unittest.TestCase):
     def test_pipeline(self):
-        data_pipeline.main("test/config.json")
+        data_pipeline.main(CONFIG_FILE)
